a984a60c271a65ab7b390154c011d207cb9018bb
Christian Keil <christian@astranis.com>
2022-03-05 19:12:33 -0800

    commit tree e92ca21bca3e16dcddf6bede97a4f9d915b695f3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 // write your code here
+const menuDiv = document.querySelector("#ramen-menu")
+const ramenDetail = document.querySelector("#ramen-detail")
+const form = document.querySelector("#new-ramen")
+
+fetch("http://localhost:3000/ramens", {
+    headers: {
+        "ContentType": "application/json",
+        "Accept": "application/json"
+    }
+})
+.then(res => res.json())
+.then(data => data.forEach(ramen => loadImage(ramen)))
+
+function loadImage (ramen) {
+    let img = document.createElement("img")
+    img.src = ramen.image
+    img.addEventListener("click", () => {
+        ramenDetail.querySelector("img").src = ramen.image
+        ramenDetail.querySelector("h2").innerText = ramen.name
+        ramenDetail.querySelector("h3").innerText = ramen.restaurant
+        document.querySelector("#rating-display").innerText = ramen.rating
+        document.querySelector("#comment-display").innerText = ramen.comment
+    })
+    menuDiv.appendChild(img)
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let newDish = {
+        "name": e.target.name.value,
+        "restaurant": e.target.restaurant.value,
+        "image": e.target.image.value,
+        "rating": e.target.rating.value,
+        "comment": e.target.comment.value
+    }
+    debugger
+    loadImage(newDish)
+})
